350a9b169a9dab778d6021afa4f61b1ccb51bae5
CaiXi <caixi169@gmail.com>
2017-01-18 00:59:49 +0800

    引用javaPeot
    使用注解生成helloworld代码

--- app/src/main/java/com/cxmax/demo/androidapt/MainActivity.java
+++ app/src/main/java/com/cxmax/demo/androidapt/MainActivity.java
@@ -3,6 +3,9 @@ package com.cxmax.demo.androidapt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.Test;
+
+@Test
 public class MainActivity extends AppCompatActivity {
 
     @Override
